51157119a644a8d6e48e02609252345efd1a3db1
Eva Tomlinson <kkheva0215@gmail.com>
2019-09-24 18:12:25 -0400

    initial commit

---
+++ A170_E01_Tomlinson_Eva/src/MyFirstJavaProgram.java
@@ -0,0 +1,15 @@
+/*
+ * Eva Tomlinson
+ * CS A170
+ * September 24, 2019
+ * 
+ * Exercise 01
+ */
+public class MyFirstJavaProgram
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is my first Java program!");
+	}
+
+}
---
+++ A170_E01_Tomlinson_Eva/src/MySecondJavaProgram.java
@@ -0,0 +1,9 @@
+
+public class MySecondJavaProgram 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("This is Eva's second Java program!");
+	}
+
+}
