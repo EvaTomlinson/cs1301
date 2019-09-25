7b6d42e05da488c7e807d770d46a5010f2427fcc
Kahee Tomlinson <kkheva0215@gmail.com>
2019-09-25 10:43:24 -0400

    added notes

--- A170_E01_Tomlinson_Eva/src/Hello.java
+++ A170_E01_Tomlinson_Eva/src/Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
-        System.out.println();
+        System.out.println("Hello, world!");//prints the given message as output.
+        System.out.println(); //prints a blank line of output.
         System.out.println("This program produces");
         System.out.println("four lines of output");
     }
