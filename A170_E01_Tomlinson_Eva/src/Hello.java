0173a2df2b6cc1d149f8b0e03841b192916bf323
Kahee Tomlinson <kkheva0215@gmail.com>
2019-09-25 10:23:10 -0400

    Hello.Java created

---
+++ A170_E01_Tomlinson_Eva/src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        System.out.println();
+        System.out.println("This program produces");
+        System.out.println("four lines of output");
+    }
+}
