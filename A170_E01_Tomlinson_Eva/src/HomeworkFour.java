a37413b1c460f0dd7c6ab6a73728f575f8753576
Kahee Tomlinson <kkheva0215@gmail.com>
2019-10-04 20:36:19 -0400

    drawing my name and pictures

--- A170_E01_Tomlinson_Eva/src/HomeworkFour.java
+++ A170_E01_Tomlinson_Eva/src/HomeworkFour.java
@@ -5,9 +5,17 @@
 // Program description:                                                                    //
 /////////////////////////////////////////////////////////////////////////////////////////////
 public class HomeworkFour
-{
-	public static void main(String[] args)
+{ //Part 1a
+  public static void main(String[] args)
 	{
+    printName();
+    System.out.println("This is my name in a 3-D effect!");
+    printPictureOne();
+    System.out.println("This is my cat Anna!");
+    printPictureTwo();
+    System.out.println("I love JAVA!");
+  }
+  public static void printName() {
     // __  __     ______     __  __     ______     ______    
     // /\ \/ /    /\  __ \   /\ \_\ \   /\  ___\   /\  ___\   
     // \ \  _"-.  \ \  __ \  \ \  __ \  \ \  __\   \ \  __\   
@@ -19,12 +27,31 @@ public class HomeworkFour
     System.out.println(" \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_\\ \\_\\  \\ \\_____\\  \\ \\_____\\");
     System.out.println("  \\/_/\\/_/   \\/_/\\/_/   \\/_/\\/_/   \\/_____/   \\/_____/");
   }
+  public static void printPictureOne() {
+    System.out.println("       ___   ___");
+    System.out.println("     // ^ \\// ^ \\\\");
+    System.out.println("   //             \\\\");
+    System.out.println("  //               \\\\");
+    System.out.println(" //    0       0    \\\\");
+    System.out.println("||         *        || ");
+    System.out.println("||                  ||");
+    System.out.println(" \\\\                //~~~");
+    System.out.println("  \\\\              //");  
+    System.out.println("    \\\\          //");
+    System.out.println("       --------- ");
+  }
+  public static void printPictureTwo() {
+    System.out.println(" 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 ");
+    System.out.println(" 000000000        000000000000        00000000000000        00000000         0000000000        00000000000 ");
+    System.out.println(" 000000000        0000000000            0000000000000        000000         000000000            000000000 ");
+    System.out.println(" 000000000        00000000       000      000000000000        0000         00000000       000      0000000 ");
+    System.out.println(" 000000000        0000000       00000      00000000000         00         00000000       00000      000000 ");
+    System.out.println(" 000000000        000000       0000000      00000000000         0        00000000       0000000      00000 ");
+    System.out.println(" 000000000        000000                    000000000000                000000000                     0000 ");
+    System.out.println(" 00000           000000       000000000      000000000000              0000000000      000000000      0000 ");
+    System.out.println(" 00000          0000000       000000000      0000000000000            00000000000      000000000      0000 ");
+    System.out.println(" 000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 ");
+
+  }
 }
 
-
-
-// public class HomeworkFourPicture {
-// 	public static void main(String[] args) {
-// 		System.out.println(" _  _  ,__\n" + "/\"\\/\"\\ :`-`\n" + "(.   .`))\n" + ".__>--<___.`\n");
-// 	}
-// }
