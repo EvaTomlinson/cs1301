1705c87b04b59324f69ad096ee04b68826ec18d4
Eva Tomlinson <kkheva0215@gmail.com>
2019-10-18 20:54:21 -0400

    simplify a statement

--- A170_E01_Tomlinson_Eva/src/CumulativeSumLoop.java
+++ A170_E01_Tomlinson_Eva/src/CumulativeSumLoop.java
@@ -9,7 +9,7 @@ class CumulativeSumLoop {
     int sum = 0;
     for (int i = 1; i <= 100; i++){
       System.out.print("Type a number: ");
-      sum = sum + console.nextInt();
+      sum += console.nextInt();
     }
     System.out.println("The sum is: " + sum);
     console.close();
