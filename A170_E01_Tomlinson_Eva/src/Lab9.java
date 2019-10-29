8261d917a3722bceb89898710209de475fd14ca3
Eva Tomlinson <kkheva0215@gmail.com>
2019-10-29 10:34:15 -0400

    using three for loops

---
+++ A170_E01_Tomlinson_Eva/src/Lab9.java
@@ -0,0 +1,30 @@
+public class Lab9 {
+  public static void main(String[] args) {
+    
+  for (int line = 1; line <= 5; line++) {
+    for (int j = 1; j <= (-1 * line + 5); j++) {
+      System.out.print(".");
+    }
+    System.out.print(line);
+  for (int j = 1; j <= (line - 1); j++) {
+    System.out.print(".");
+  }
+  System.out.println();
+    }
+  }
+}
+
+// public class Lab9 {
+//   public static void main(String[] args) {
+    
+//   for (int line = 1; line <= 5; line++) {
+//     for (int j = 1; j <= (-1 * line + 5); j++) {
+//       System.out.print(".");
+//     }
+//   for (int j = 1; j <= line; j++) {
+//     System.out.print(line);
+//   }
+//   System.out.println();
+//     }
+//   }
+// }
