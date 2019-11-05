d83bc26aab264aaebe511dea2d80c2f8af5f27cf
Eva Tomlinson <kkheva0215@gmail.com>
2019-11-05 11:40:49 -0500

    using for loop

---
+++ A170_E01_Tomlinson_Eva/src/Lab10.java
@@ -0,0 +1,11 @@
+public class Lab10 {
+  public static void main(String[] args) {
+    for (int i = 0; i <= 5; i++) {
+      //System.out.println(i);
+      for (int j = 0; j <= 5; j++) {
+        System.out.print("(" + i + "," + j + ") ");
+      }
+      System.out.println();
+    }
+  }
+}
